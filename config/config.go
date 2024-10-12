136b18cc836ae5b1b8f23b67bb704109c09f184f
kazuddd <daud.oscc@gmail.com>
2024-10-12 15:43:30 +0700

    [fix] Changed credentials location

--- config/config.go
+++ config/config.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,11 +13,28 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/syncspend?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get environment variables
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Data source name (DSN) format
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, dbName)
+
+	// Connect to the database
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
+
 	log.Println("Database connection successful")
 }
