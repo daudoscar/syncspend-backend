e50aec50b141488969e2cc2ca1e6536daa21fa03
kazuddd <daud.oscc@gmail.com>
2024-10-20 21:38:30 +0700

    [feat] Implemented plan model

---
+++ models/plan.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Plan struct {
+	ID                uint64    `gorm:"primaryKey;autoIncrement"`
+	ID_Owner          string    `gorm:"type:varchar(255);not null"`
+	Title             string    `gorm:"type:varchar(255)"`
+	Description       string    `gorm:"type:varchar(255);unique;not null"`
+	InviteCode        string    `gorm:"type:varchar(6)"`
+	InviteCodeExpires time.Time `gorm:"type:datetime"`
+	IsDeleted         bool      `gorm:"default:false"`
+	CreatedAt         time.Time `gorm:"autoCreateTime"`
+	UpdatedAt         time.Time `gorm:"autoUpdateTime"`
+}
